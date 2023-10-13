2bb8d2e4e1d24457f4d084dc7477405bb41f1eff
NaitouT-A <132803168+NaitouT-A@users.noreply.github.com>
2023-10-13 11:13:05 +0900

    文字化け対応

--- Task_Game2.cpp
+++ Task_Game2.cpp
@@ -1,25 +1,25 @@
-//?X?e?[?W?Q?iVs?????j
+//ステージ２（Vs魔女）
 #include"Task_Game2.h"
 #include"Task_EventEngine.h"
 namespace Game2 {
-	Object::Object(CManager* pManager) : BTask(pManager)//?R???X?g???N?^
+	Object::Object(CManager* pManager) : BTask(pManager)//コンストラクタ
 	{
-		//?Q?[?????????????
+		//ゲームの状態を通常にする
 		ge->gamestate = MyGE::MyGameEngine::GameState::Non;		
-		//???
+		//再生
 		ge->PlayMusicLoop(music, 70);
-		//?w?i?????
+		//背景の生成
 		auto bg = GameBG::Object::Create(pManager);
-		//?Q?[????????
+		//ゲーム盤の生成
 		for (int y=0;y<chipsizeY;y++)
 		{
 			for (int x = 0; x < chipsizeX; x++)
 			{
 				auto board = Board::Object::Create(pManager);
-				board->Setter(VGet(650+x * 180,300+y*190, 0));
+				board->Setter(VGet(650+x * 180,450+y*190, 0));
 			}
 		}
-		//?Q?[???????W
+		//ゲーム盤の座標
 		VECTOR Prepos[chipsizeY][chipsizeX];
 		for (int y = 0; y < chipsizeY; y++)
 		{
@@ -31,10 +31,10 @@ namespace Game2 {
 		}
 
 		int x=0, y=0;
-		auto target = ge->Find<Board::Object>("?Q?[????");
+		auto target = ge->Find<Board::Object>("ゲーム盤");
 		for (auto it = target->begin(); it != target->end(); ++it)
 		{
-			Prepos[y][x] = (*it)->Getter();//?{?[?h??}?X????W
+			Prepos[y][x] = (*it)->Getter();//ボードのマスの座標
 			x++;
 			if(x>2)
 			{
@@ -43,38 +43,38 @@ namespace Game2 {
 			}
 		}
 
-		//?v???C???????
+		//プレイヤの生成
 		auto pl = Player::Object::Create(pManager);
 		pl->SetBoardPos(Prepos);
 		pl->Setter(Prepos[1][1]);
 
 
-		//?v???C??HP?????	
+		//プレイヤHPの生成	
 		for (int i=0;i<playerHp;++i)
 		{
 			auto php = PlayerHp::Object::Create(pManager);	
 		}
-		auto targets2 = ge->Find<PlayerHp::Object>("?v???C??HP");
+		auto targets2 = ge->Find<PlayerHp::Object>("プレイヤHP");
 		int i=0,n=0;
 		for (auto it = targets2->begin(); it != targets2->end(); it++)
 		{
 				(*it)->Setter(VGet(1550+i * 64, 900, 0));
 				i++;
 		}
-		//?G?????
+		//敵の生成
 		auto en = Enemy::Object::Create(pManager);
 
-		auto Enemy = ge->Find<Enemy::Object>("?G");
+		auto Enemy = ge->Find<Enemy::Object>("敵");
 		auto it1 = Enemy->begin();
 		(*it1)->SetBoardPos(Prepos);
 
-		//?GHP?????	
+		//敵HPの生成	
 		for (int i = 0; i < witchHp; ++i)
 		{
 			auto ehp = EnemyHp::Object::Create(pManager);	
 		}
 
-		auto enemyHp = ge->Find<EnemyHp::Object>("?G?l?~?[HP");
+		auto enemyHp = ge->Find<EnemyHp::Object>("エネミーHP");
 		i = 0;
 		for (auto it = enemyHp->begin(); it != enemyHp->end(); it++)
 		{
@@ -86,11 +86,11 @@ namespace Game2 {
 			(*it)->Setter(VGet(100 +i * 64, (n*64)+100, 0));
 			i++;
 		}
-		//?Q?[???J?n???t???O
+		//ゲーム開始時フラグ
 		this->GameStart = false;
 	}
 
-	Object::~Object()//?f?X?g???N?^ 
+	Object::~Object()//デストラクタ 
 	{
 		DeleteSoundMem(music);
 	}
@@ -107,40 +107,40 @@ namespace Game2 {
 			this->GameStart = true;
 		}
 
-		auto targets = ge->Find<Player::Object>("?v???C??");
+		auto targets = ge->Find<Player::Object>("プレイヤ");
 		auto it = targets->begin();
 		if ((*it)->motion == BChara::Motion::Dead && ge->gamestate == MyGE::MyGameEngine::GameState::Non)
 		{
 			(*it)->Dead();
-			ge->KillAll_G("?_???[?WUI");
-			ge->KillAll_G("?G");
-			ge->KillAll_G("?Q?[????");
-			ge->KillAll_G("?w?i");
-			ge->KillAll_G("?v???C??HP");
-			ge->KillAll_G("?G?l?~?[HP");
-			ge->KillAll_G("?v???C??");
+			ge->KillAll_G("ダメージUI");
+			ge->KillAll_G("敵");
+			ge->KillAll_G("ゲーム盤");
+			ge->KillAll_G("背景");
+			ge->KillAll_G("プレイヤHP");
+			ge->KillAll_G("エネミーHP");
+			ge->KillAll_G("プレイヤ");
 
 			ge->gamestate = MyGE::MyGameEngine::GameState::GameOver;
-			//?Q?[????V?[????s
+			//ゲームにシーン移行
 			manager->scene = new GameOver::Object(manager);
 			delete this;
 		}
 		if (ge->gamestate != MyGE::MyGameEngine::GameState::GameOver)
 		{
-			auto targets2 = ge->Find<Enemy::Object>("?G");
+			auto targets2 = ge->Find<Enemy::Object>("敵");
 			auto it2 = targets2->begin();
 			if ((*it2)->motion == BChara::Motion::Dead && ge->gamestate == MyGE::MyGameEngine::GameState::Non)
 			{
 				(*it2)->Dead();
-				ge->KillAll_G("?_???[?WUI");
-				ge->KillAll_G("?G");
-				ge->KillAll_G("?Q?[????");
-				ge->KillAll_G("?w?i");
-				ge->KillAll_G("?v???C??HP");
-				ge->KillAll_G("?G?l?~?[HP");
-				ge->KillAll_G("?v???C??");
+				ge->KillAll_G("ダメージUI");
+				ge->KillAll_G("敵");
+				ge->KillAll_G("ゲーム盤");
+				ge->KillAll_G("背景");
+				ge->KillAll_G("プレイヤHP");
+				ge->KillAll_G("エネミーHP");
+				ge->KillAll_G("プレイヤ");
 				ge->gamestate = MyGE::MyGameEngine::GameState::GameClear;
-				//?Q?[????V?[????s
+				//ゲームにシーン移行
 				manager->scene = new GameClear::Object(manager);
 				delete this;
 			}
